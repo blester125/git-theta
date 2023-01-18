d97039ec1c9ec35b265213074aa6fbf367555132
Brian Lester <blester125@gmail.com>
2023-01-18 00:04:30 -0500

    ups

--- git_theta/checkpoints/t5x_checkpoint.py
+++ git_theta/checkpoints/t5x_checkpoint.py
@@ -1,10 +1,18 @@
 """Handle T5X checkpoints."""
 
 from git_theta.checkpoints import Checkpoint
+from t5x import checkpoints as t5x_checkpoints
 
 
 class T5XCheckpoint(Checkpoint):
-
     @property
     def name(self):
-        return "t5x
+        return "t5x"
+
+    @classmethod
+    def load(cls, checkpoint_path):
+        return t5x_checkpoints.load_t5x_checkpoint(checkpoint_path)
+
+    def save(self, checkpoint_path):
+        # TODO: How to output the checkpoint when things are partitioned?
+        ...
